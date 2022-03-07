68233f2eaed60dfca7cba201c331fcef001613e3
v2stech <v2stech@v2stech-Latitude-3520>
2022-03-07 18:18:31 +0530

    commit march 7 2022

--- Cruise_booking_system/src/main/java/Exam/Cruise_booking_system/All_Func.java
+++ Cruise_booking_system/src/main/java/Exam/Cruise_booking_system/All_Func.java
@@ -1,75 +1,122 @@
 package Exam.Cruise_booking_system;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
+import java.io.*;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.ObjectInput;
-import java.io.ObjectInputStream;
-import java.io.Writer;
-import java.nio.Buffer;
+
 import java.util.List;
 import java.util.Scanner;
 
-import com.opencsv.bean.*;
-import com.opencsv.exceptions.CsvDataTypeMismatchException;
-import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
-import com.opencsv.CSVReader;
+
+
+
+
+class emailexp extends Exception{
+	emailexp(){
+		System.out.println("wrongs email id");
+		
+	}
+}
+
+class idexp extends Exception{
+	idexp(){
+		System.out.println("wrongs customer id");
+		
+	}
+}
+
+
+
+
 
 public class All_Func {
 
-	String fileName = "src/main/resources/customer_data.csv";
-	int flag = 0,d,m,y;
+	static String fn= "customer_data.csv";
+	static String fileName = "src/main/resources/"+fn;
+	int flag ,d,m,y;
 	static Scanner sc = new Scanner(System.in);
-	CrusieData cdi = new CrusieData();
+	
+	
+	public void filechecker(String file) throws emailexp {
+		if(fn.equals(file)) {
+			System.out.print("\ndata read sucessfully\n");
+		}
+		else {
+			System.out.println("enter correct file name try again: ");
+			App.f=sc.next();
+			System.out.print("\ndata read sucessfully\n");
+			
+		}
+	}
 
-	public List<AllData> csv_reader(List<AllData> a) {
+	public List<AllData> csv_reader(List<AllData> a) throws IOException, emailexp {
 
-		try {
 			BufferedReader br = new BufferedReader(new FileReader(fileName));
 			String ts = "";
 			while ((ts = br.readLine()) != null) {
 				String[] s = ts.split(",");
-				AllData a1 = new AllData(s[0], s[1], s[2]);
-				a.add(a1);
+				String c="@gmail.com";
+				String c_id=s[0];
+				String e=s[2];
+				if(e.endsWith(c) & e.charAt(0)>='a'& e.charAt(0)<='z' & c_id.charAt(0)=='c') {
+					AllData a1 = new AllData(s[0], s[1], s[2]);
+					a.add(a1);
+				}
+				else {
+//					throw new idexp();    //cant check 2 exception at once
+					throw new emailexp();
+				}
+				
 			}
+			
+			return a;
 
-		} catch (Exception e) {
-			System.out.println(e);
-		}
+		} 
 
-		return a;
+		
 
-	}
+	public void search_cust(String search, List<AllData> a) {
 
-	public String search_cust(String search, List<AllData> a) {
-
-		String id = "", name = "", s = "";
+		flag=0;
 		for (AllData ad1 : a) {
 
-			if (search.equals(ad1.getCust_name())) {
+			if (ad1.getCust_name().contains(search)) {
 				flag = 1;
-				id = ad1.getCust_id();
-				name = ad1.getCust_name();
+				System.out.println("id = "+ad1.getCust_id()+" name = "+ad1.getCust_name());
+			}
+
+		}
+		if (flag == 0) {
+			System.out.println("\nnot found");  
+		} 
+		
+		
+	}
+	
+	public void search_cust_id(String id, List<AllData> a) {
+
+		int flag=0;
+		for (AllData ad1 : a) {
+
+			if (id.equals(ad1.getCust_id())) {
+				flag=1;
+				System.out.println("id = "+ad1.getCust_id()+" name = "+ad1.getCust_name());
 
 			}
 
 		}
-		if (flag == 1) {
-			s = "\n**************name found***********  " + "\ncust id: " + id + "\tcust_name: " + name;
-		} else {
-			s = "\nnot found";
-		}
-
-		return s;
+		if (flag == 0) {
+			System.out.println("\nnot found");  
+		} 
 
 	}
-	
 	
 	public void book_cruz(String funame,List<AllData> a,List<CrusieData> c) {
 		String as = "", ti = "", da = "";
 
+		flag=0;
 		for (AllData ad2 : a) {
 
 			if (ad2.getCust_name().equals(funame)) {
@@ -104,12 +151,14 @@ public class All_Func {
 					y = sc.nextInt();
 				}
 				da = d + "/" + m + "/" + y;
+				CrusieData cdi = new CrusieData();
+				cdi.setC_id(ti);
+				cdi.setCname(funame);
+				cdi.setDate(da);
+				c.add(cdi);
 
 			}
-			cdi.setC_id(ti);
-			cdi.setCname(funame);
-			cdi.setDate(da);
-			c.add(cdi);
+			
 
 		}
 
@@ -123,6 +172,7 @@ public class All_Func {
 	
 	
 	public void cust_cruz(List<CrusieData> c) {
+		flag=0;
 		String dat = "";
 		System.out.print("\nenter the date ");
 		System.out.print("\n\tenter booking day: ");
@@ -153,56 +203,92 @@ public class All_Func {
 			y = sc.nextInt();
 		}
 		dat = d + "/" + m + "/" + y;
-		String cbook="";
 		int counter=0;
+		
 		for (CrusieData cd1 : c) {
 
 			if (cd1.getDate().equals(dat)) {
 				flag = 1;
 				counter++;
-//				for(CrusieData cd5:c)
-//					{
-//					  cbook+="\n "+"cust id: " + cd5.getC_id() + "\tcust_name: " + cd5.getCname();
-//					}
-				 cbook+="\n "+"cust id: " + cd1.getC_id() + "\tcust_name: " + cd1.getCname();
-					
-
+				System.out.println("\ncust id: " + cd1.getC_id() + "\tcust_name: " + cd1.getCname());
+				
 			}
-			break;
 
-		}
-		if (flag == 1) {
-			System.out.print(cbook);
-			System.out.print("\nnumber of attendee :"+counter);
-		} else {
-			System.out.print("\number attendee not present");
 		}
 		
+		System.out.print("\nnumber of attendee :"+counter);
+		if (flag == 0) {
+			
+			System.out.print("\number attendee not present");
+		} 
 		
 	}
 	
 	
+	public void file_writer(List<CrusieData> c) throws IOException {
+		flag=0;
+		String date1 = "";
+		System.out.print("\nenter the date ");
+		System.out.print("\n\tenter booking day: ");
+		 d = sc.nextInt();
+		if (d > 0 & d <= 31) {
+			d = d;
+		} else {
+			System.out.print(
+					"***please enter value between month days that is from 1 to 31*****\n\tenter oppointment day: ");
+			d = sc.nextInt();
+		}
+		System.out.print("\n\tenter booking month: ");
+		 m = sc.nextInt();
+		if (m > 0 & m <= 12) {
+			m = m;
+		} else {
+			System.out.print(
+					"***please enter value for month that is from 1 to 12*****\n\tenter oppointment month: ");
+			m = sc.nextInt();
+		}
+		System.out.print("\n\tenter booking year: ");
+		 y = sc.nextInt();
+		if (y > 2021 & y <= 2025) {
+			y = y;
+		} else {
+			System.out.print(
+					"***please enter value for year that is from 2022 to 2030*****\n\tenter oppointment year: ");
+			y = sc.nextInt();
+		}
+		date1 = d + "/" + m + "/" + y;
+		
+		String Da=d+"_"+m+"_"+y;
+		
+		
+		
+		
+		String cdate=date1;
+		String path = "src/main/resources/cruise"+Da+".csv";
+		File fi = new File(path);
+		
+		FileWriter w = new FileWriter(fi); 
+		for (CrusieData cd2 : c) {
+
+
+			
+			if (cd2.getDate().equals(date1)) {					
+				 
+					w.append(cd2.getC_id());
+					w.append(",");
+					w.append(cd2.getCname());
+					w.append(",");
+					w.append(cd2.getDate()+"\n"); 
+				
+				}
+				
+		}
+		w.close();
+	}
 	
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 	
 	
 	
--- Cruise_booking_system/src/main/java/Exam/Cruise_booking_system/App.java
+++ Cruise_booking_system/src/main/java/Exam/Cruise_booking_system/App.java
@@ -9,133 +9,127 @@ import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
 
 
 /**
- * int b= 50; switch(b) { case 20:System.out.println("20 exe");break; case
- * 30:System.out.println("30 exe");break; case 40:System.out.println("40
- * exe");break; default:System.out.println("no one exe"); }
+ *
+ * customer_data.csv
  *
  */
+
+//
+
+
+
+
 public class App {
-	static String fileName = "src/main/resources/customer_data.csv";
+	
 	static Scanner sc = new Scanner(System.in);
-
-	public class cruiseab {
-
-	}
-
 	static List<AllData> a = new ArrayList<AllData>();
 	static List<CrusieData> c = new ArrayList<CrusieData>();
+	
+	public static String f;
 
-	public static void main(String[] args) throws IOException, CsvDataTypeMismatchException, CsvRequiredFieldEmptyException {
+	public static void main(String[] args) throws IOException, CsvDataTypeMismatchException, CsvRequiredFieldEmptyException, emailexp {
 		All_Func af = new All_Func();
 		System.out.println("---------------cruise booking system------------");
-		int ch = 0 ; // choice for operartion
+		
 
 		CrusieData cd = new CrusieData();
-
+		 int ch=0;
 		do {
 			int flag = 0,d=0,m=0,y=0;
 			System.out.print(
-					"\n[1]read cust data into list from file\n[2]display all cust id and name list\n[3]search cust list\n[4]allow cust to book cruise\n[5]view cust booked cruise\n[6]save cust to file\n[0]exit\n");
+					"\n[1]read cust data into list from file\n[2]display all cust id and name list\n[3]search cust by name in list\n[4]search cust by id in list\n[5]allow cust to book cruise\n[6]view cust booked cruise\n[7]save cust to file\n[0]exit\n");
 
 			System.out.print("\n enter choice: ");
-			ch = sc.nextInt();
-
-			switch (ch) {
-			case 1:
-				a = af.csv_reader(a);
-				System.out.print("\ndata read sucessfully\n");
-				break;
-			case 2:
-				System.out.print("\ncust id\t cust name\n");
-
-				for (AllData ad1 : a) {
-					System.out.print("\n" + ad1.getCust_id() + "\t " + ad1.getCust_name());
-				}
-				System.out.print("\n");
-				break;
-			case 3:
-				System.out.print("\neneter customer name you want search : ");
-				String fname = sc.next();
-				String sname = sc.nextLine();
-				String search = fname + sname;
-				String ans = af.search_cust(search, a);
-				System.out.println(ans);
-
-				break;
-			case 4:
-				String as = "", ti = "", da = "";
-				System.out.print("enter the name ");
-				String f1name = sc.next();
-				String s2name = sc.nextLine();
-				String funame = f1name + s2name;
-				af.book_cruz(funame,a,c);
-
-				break;
-			case 5:
-				System.out.print("\nview cust booked cruise");
-				af.cust_cruz(c);
+				ch=sc.nextInt();
 				
-
-				break;
-			case 6:
-				String date1 = "";
-				System.out.print("\nenter the date ");
-				System.out.print("\n\tenter booking day: ");
-				 d = sc.nextInt();
-				if (d > 0 & d <= 31) {
-					d = d;
-				} else {
-					System.out.print(
-							"***please enter value between month days that is from 1 to 31*****\n\tenter oppointment day: ");
-					d = sc.nextInt();
-				}
-				System.out.print("\n\tenter booking month: ");
-				 m = sc.nextInt();
-				if (m > 0 & m <= 12) {
-					m = m;
-				} else {
-					System.out.print(
-							"***please enter value for month that is from 1 to 12*****\n\tenter oppointment month: ");
-					m = sc.nextInt();
-				}
-				System.out.print("\n\tenter booking year: ");
-				 y = sc.nextInt();
-				if (y > 2021 & y <= 2025) {
-					y = y;
-				} else {
-					System.out.print(
-							"***please enter value for year that is from 2022 to 2030*****\n\tenter oppointment year: ");
-					y = sc.nextInt();
-				}
-				date1 = d + "/" + m + "/" + y;
-				
-				String path = ("src/main/resources/cruise_opp.csv");
-				for (CrusieData cd2 : c) {
-
-
-					FileWriter w = new FileWriter(path);  
-					if (cd2.getDate().equals(date1)) {					
-						
-							w.append(cd2.getC_id());
-							w.append(",");
-							w.append(cd2.getCname());
-							w.append(",");
-							w.append(cd2.getDate()+"\n"); 
-							
-						}
-						w.close();
+				switch (ch) {
+				case 1:
+					
+					System.out.println("enter file name");
+					String f=sc.next();
+					af.filechecker(f);
+					a = af.csv_reader(a);
+					System.out.println("no of customers are : "+ a.size());
 					break;
-							
+				case 2:
+					if(!a.isEmpty()) {
+						for (AllData ad1 : a) {
+							System.out.print("\n" + ad1.getCust_id() + "\t " + ad1.getCust_name());
+						}
+						System.out.print("\n");
+					}
+					else {
+						System.out.print("\nenter wrong input please choose option 1\n");
+					}
+					
+					break;
+				case 3:
+					if(!a.isEmpty()) {
+						System.out.print("\neneter customer name you want search : ");
+						String fname = sc.next();
+						String sname = sc.nextLine();
+						String search = fname + sname;
+						af.search_cust(search, a);
+					
+					}
+					else {
+						System.out.print("\nenter wrong input please choose option 1\n");
+					}
+					
 
+					break;
+				case 4:
+					if(!a.isEmpty()) {
+						System.out.print("\neneter customer id you want search : ");
+						String id=sc.next();
+						af.search_cust_id(id,a);
+					
+					}
+					else {
+						System.out.print("\nenter wrong input please choose option 1\n");
+					}
+
+					break;
+				case 5:
+					if(!a.isEmpty()) {
+						String as = "", ti = "", da = "";
+						System.out.print("enter the name ");
+						String f1name = sc.next();
+						String s2name = sc.nextLine();
+						String funame = f1name + s2name;
+						af.book_cruz(funame,a,c);
+					
+					}
+					else {
+						System.out.print("\nenter wrong input please choose option 1\n");
+					}
+
+					break;
+				case 6:
+					if(!a.isEmpty()) {
+						System.out.print("\nview cust booked cruise");
+						af.cust_cruz(c);
+					}
+					else {
+						System.out.print("\nenter wrong input please choose option 1\n");
+					}
+					
+					break;
+				case 7:
+					if(!a.isEmpty()) {
+						af.file_writer(c);
+						System.out.print("data write suscessfull");
+					
+					}
+					else {
+						System.out.print("\nenter wrong input please choose option 1\n");
+					}
+					break;
+
+				case 0:
+					System.out.println("thank you!!");break;
 				}
-				System.out.print("data write suscessfull");
 				
-				
-				break;
-
-			case 0:
-				System.out.println("thank you!!");break;
-			}
 
 		} while (ch != 0);
 
